2d1ad4eaea277136a1e294ad72b129f5d46035ae
siriak <siryaka@gmail.com>
2019-01-18 09:32:10 +0200

    Add Unit

--- Ascension.Domain/Concepts/Unit.cs
+++ Ascension.Domain/Concepts/Unit.cs
@@ -2,5 +2,18 @@
 {
     public abstract class Unit
     {
+        public abstract string Name { get; protected set; }
+
+        public abstract uint Damage { get; protected set; }
+
+        public abstract UnitType DamageType { get; protected set; }
+
+        public abstract uint Health { get; protected set; }
+
+        public abstract UnitType HealthType { get; protected set; }
+
+        public abstract Resources HireCost { get; protected set; }
+
+        public abstract Resources MaintenanceCost { get; protected set; }
     }
 }
