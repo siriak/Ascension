f58e4c802b44bd74917d170d10579a9ee8808473
siriak <siryaka@gmail.com>
2019-01-25 10:53:11 +0200

    Add unit stack

---
+++ Ascension.Domain.Tests/Concepts/UnitStackTests.cs
@@ -0,0 +1,80 @@
+﻿using Ascension.Domain.Concepts;
+using Ascension.Domain.Units;
+using NUnit.Framework;
+
+namespace Ascension.Domain.Tests.Concepts
+{
+    [TestFixture]
+    public class UnitStackTests
+    {
+        [Test]
+        [Parallelizable]
+        public void ReceiveDamage_StackDestroyedRightDamage_UnitCountIsZero([Random(1)]uint count)
+        {
+            var (specification, stack) = SetUp(count);
+
+            stack.ReceiveDamage(specification.Health * count);
+
+            Assert.AreEqual(0, stack.UnitCount);
+        }
+
+        [Test]
+        [Parallelizable]
+        public void ReceiveDamage_StackDestroyedMoreDamage_UnitCountIsZero([Random(1)]uint count)
+        {
+            var (specification, stack) = SetUp(count);
+
+            stack.ReceiveDamage(specification.Health * (count + 1));
+
+            Assert.AreEqual(0, stack.UnitCount);
+        }
+
+        [Test]
+        [Parallelizable]
+        public void ReceiveDamage_StackNotDestroyed1HPLeft_UnitCountNotZero([Random(1, uint.MaxValue, 1)]uint count)
+        {
+            var (specification, stack) = SetUp(count);
+
+            stack.ReceiveDamage(specification.Health * count - 1);
+
+            Assert.AreNotEqual(0, stack.UnitCount);
+        }
+
+        [Test]
+        [Parallelizable]
+        public void ReceiveDamage_StackNotDestroyed1UnitLeft_UnitCountNotZero([Random(1, uint.MaxValue, 1)]uint count)
+        {
+            var (specification, stack) = SetUp(count);
+
+            stack.ReceiveDamage(specification.Health * (count - 1));
+
+            Assert.AreNotEqual(0, stack.UnitCount);
+        }
+
+        [Test]
+        [Parallelizable]
+        public void IsEmpty_UnitCountIsZero_StackIsEmpty()
+        {
+            var (_, stack) = SetUp(0);
+
+            Assert.IsTrue(stack.IsEmpty);
+        }
+
+        [Test]
+        [Parallelizable]
+        public void IsEmpty_UnitCountNotZero_StackNotEmpty([Random(1, uint.MaxValue, 1)]uint count)
+        {
+            var (_, stack) = SetUp(count);
+
+            Assert.IsFalse(stack.IsEmpty);
+        }
+
+        private (UnitSpecification specification, UnitStack stack) SetUp(uint count)
+        {
+            var specification = new SlaveSpecification();
+            var stack = new UnitStack(specification, count);
+
+            return (specification, stack);
+        }
+    }
+}
---
+++ Ascension.Domain/Concepts/UnitStack.cs
@@ -0,0 +1,23 @@
+﻿namespace Ascension.Domain.Concepts
+{
+    public class UnitStack
+    {
+        public UnitStack(UnitSpecification unitSpecification, uint unitCount = 0)
+        {
+            UnitSpecification = unitSpecification;
+            UnitCount = unitCount;
+        }
+
+        public UnitSpecification UnitSpecification { get; }
+
+        public bool IsEmpty => UnitCount == 0;
+
+        public uint UnitCount { get; private set; }
+
+        public void ReceiveDamage(uint damage)
+        {
+            var lostCount = damage / UnitSpecification.Health;
+            UnitCount = lostCount > UnitCount ? 0 : UnitCount - lostCount;
+        }
+    }
+}
