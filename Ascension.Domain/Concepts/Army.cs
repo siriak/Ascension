8c16a08c4d3b6eab4383adf18f0eac022f72df5d
siriak <siryaka@gmail.com>
2019-01-26 21:16:36 +0200

    Add stacks to army

--- Ascension.Domain/Concepts/Army.cs
+++ Ascension.Domain/Concepts/Army.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Ascension.Domain.Concepts
 {
     public abstract class Army
     {
-        public ICollection<(Unit, int count)> Units { get; }
+        public ISet<UnitStack> Units { get; }
+
+        public IEnumerable<UnitStack> GetUnitStacksOfAttackType(TargetUnitClass targetUnitClass) => Units.Where(s => s.UnitSpecification.TargetUnitClass == targetUnitClass);
+
+        public IEnumerable<UnitStack> GetUnitStacksOfDefenseType(UnitClass unitClass) => Units.Where(s => s.UnitSpecification.UnitClass == unitClass);
     }
 }
