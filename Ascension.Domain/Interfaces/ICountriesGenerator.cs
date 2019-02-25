c4a58e7531e87f6fd3abf907e7ec25f7f684030b
siriak <siryaka@gmail.com>
2019-02-25 14:52:33 +0200

    Add generator interfaces

---
+++ Ascension.Domain/Interfaces/ICountriesGenerator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Ascension.Domain.Concepts
+{
+    internal interface ICountriesGenerator
+    {
+        ICollection<Country> GenerateCountries();
+    }
+}
---
+++ Ascension.Domain/Interfaces/IMapGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+using Ascension.Domain.Concepts;
+
+namespace Ascension.Domain.Interfaces
+{
+    internal interface IMapGenerator
+    {
+        Map GenerateMap(ICollection<Country> countries);
+    }
+}
