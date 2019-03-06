f7bda69f37b8309872b5b8d3903eb37d1b2f5929
siriak <siryaka@gmail.com>
2019-03-06 21:35:58 +0200

    Add Factory building

---
+++ Ascension.Domain/Buildings/Factory.cs
@@ -0,0 +1,31 @@
+﻿using Ascension.Domain.Concepts;
+
+namespace Ascension.Domain.Buildings
+{
+    public class Factory : Building
+    {
+        public Factory(Territory territory)
+            : base(territory)
+        {
+        }
+
+        public override Resources Income => Territory.Surface == Surface.Hills
+            ? new Resources
+            {
+                Production = 150,
+                Commerce = 10,
+                Science = 10,
+            }
+            : new Resources
+            {
+                Production = 100,
+                Commerce = 10,
+                Science = 10,
+            };
+
+        public override Resources Capacity => new Resources
+        {
+            Food = 10,
+        };
+    }
+}
