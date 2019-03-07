8ca7e209af8dfc42bf8a20ea4024b4dcbfc0c33c
siriak <siryaka@gmail.com>
2019-03-07 21:59:27 +0200

    Add farm building

---
+++ Ascension.Domain/Buildings/Farm.cs
@@ -0,0 +1,22 @@
+﻿using Ascension.Domain.Concepts;
+
+namespace Ascension.Domain.Buildings
+{
+    public class Farm : Building
+    {
+        public Farm(Territory territory)
+            : base(territory)
+        {
+        }
+
+        public override Resources Income => new Resources
+        {
+            Food = 100,
+        };
+
+        public override Resources Capacity => new Resources
+        {
+            Food = 1000,
+        };
+    }
+}
