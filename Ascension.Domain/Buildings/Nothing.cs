26de1c758d2b85b2e61b472daca018032a2b3261
siriak <siryaka@gmail.com>
2019-03-08 08:45:43 +0200

    Add Nothing building

---
+++ Ascension.Domain/Buildings/Nothing.cs
@@ -0,0 +1,22 @@
+﻿using Ascension.Domain.Concepts;
+
+namespace Ascension.Domain.Buildings
+{
+    public class Nothing : Building
+    {
+        public Nothing(Territory territory)
+            : base(territory)
+        {
+        }
+
+        public override Resources Income => new Resources()
+        {
+            Food = 10,
+            Production = 10,
+            Science = 20,
+            Culture = 20,
+        };
+
+        public override Resources Capacity => default;
+    }
+}
