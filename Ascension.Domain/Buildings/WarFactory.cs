e7d5a7d47c4f070c39903630c6fb90d558eacbf4
siriak <siryaka@gmail.com>
2019-03-09 14:27:14 +0200

    Add WarFactory building

---
+++ Ascension.Domain/Buildings/WarFactory.cs
@@ -0,0 +1,31 @@
+﻿using Ascension.Domain.Concepts;
+
+namespace Ascension.Domain.Buildings
+{
+    public class WarFactory : Building
+    {
+        public WarFactory(Territory territory)
+            : base(territory)
+        {
+        }
+
+        public override Resources Income => Territory.Surface == Surface.Hills
+            ? new Resources
+            {
+                Ammunition = 150,
+                Commerce = 10,
+                Science = 10,
+            }
+            : new Resources
+            {
+                Ammunition = 100,
+                Commerce = 10,
+                Science = 10,
+            };
+
+        public override Resources Capacity => new Resources
+        {
+            Food = 10,
+        };
+    }
+}
