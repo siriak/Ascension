4c770b3037ce79cb129de4a39b73edb8d9a1a62c
siriak <siryaka@gmail.com>
2019-03-11 14:17:02 +0200

    Add GlobalProject

---
+++ Ascension.Domain/Projects/GlobalProject.cs
@@ -0,0 +1,12 @@
+﻿using Ascension.Domain.Concepts;
+
+namespace Ascension.Domain.Projects
+{
+    public abstract class GlobalProject : Project
+    {
+        public GlobalProject(Country country, Resources cost)
+            : base(cost) => Country = country;
+
+        public Country Country { get; }
+    }
+}
