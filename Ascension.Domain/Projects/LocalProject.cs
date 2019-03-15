2847f7fe6097fd5f9e21fb234aa99056730b99e4
siriak <siryaka@gmail.com>
2019-03-15 18:56:33 +0200

    Add local projects

---
+++ Ascension.Domain/Projects/LocalProject.cs
@@ -0,0 +1,12 @@
+﻿using Ascension.Domain.Concepts;
+
+namespace Ascension.Domain.Projects
+{
+    public abstract class LocalProject : Project
+    {
+        public LocalProject(Territory territory, Resources cost)
+            : base(cost) => Territory = territory;
+
+        public Territory Territory { get; }
+    }
+}
