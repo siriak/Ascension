51d50c8937b118a270184fb7c4e01b8596a07a80
siriak <siryaka@gmail.com>
2019-03-16 18:57:52 +0200

    Add build capital project

---
+++ Ascension.Domain/Projects/Local/BuildCapital.cs
@@ -0,0 +1,15 @@
+﻿using Ascension.Domain.Buildings;
+using Ascension.Domain.Concepts;
+
+namespace Ascension.Domain.Projects.Local
+{
+    public class BuildCapital : LocalProject
+    {
+        public BuildCapital(Territory territory)
+            : base(territory, new Resources() { Production = 1000 })
+        {
+        }
+
+        protected override void OnProjectFinished() => Territory.Building = new Capital(Territory);
+    }
+}
