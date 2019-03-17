d2fb97cce016b1c3680f3c7e8bf0113b40608970
siriak <siryaka@gmail.com>
2019-03-17 07:33:21 +0200

    Add build farm project

---
+++ Ascension.Domain/Projects/Local/BuildFarm.cs
@@ -0,0 +1,15 @@
+﻿using Ascension.Domain.Buildings;
+using Ascension.Domain.Concepts;
+
+namespace Ascension.Domain.Projects.Local
+{
+    public class BuildFarm : LocalProject
+    {
+        public BuildFarm(Territory territory)
+            : base(territory, new Resources() { Production = 100 })
+        {
+        }
+
+        protected override void OnProjectFinished() => Territory.Building = new Farm(Territory);
+    }
+}
