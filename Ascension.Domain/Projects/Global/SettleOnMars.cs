ef1385b66f82c9fc25b56d760d5ec477309eef59
siriak <siryaka@gmail.com>
2019-03-25 22:04:05 +0200

    Add settle on Mars project

---
+++ Ascension.Domain/Projects/Global/SettleOnMars.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Ascension.Domain.Concepts;
+
+namespace Ascension.Domain.Projects.Global
+{
+    public class SettleOnMars : GlobalProject
+    {
+        public SettleOnMars(Country country)
+            : base(country, new Resources() { Production = 1000000 })
+        {
+        }
+
+        protected override void OnProjectFinished() => throw new NotImplementedException();
+    }
+}
