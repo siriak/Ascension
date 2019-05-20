073bf36554d0283c6b58460d30e7b53467c67dd2
siriak <siryaka@gmail.com>
2019-05-20 22:05:35 +0300

    Downgrade to .net core 2.1

--- Ascension.WebApi/Startup.cs
+++ Ascension.WebApi/Startup.cs
@@ -12,7 +12,7 @@ namespace Ascension.WebApi
 
         public IConfiguration Configuration { get; }
 
-        public void ConfigureServices(IServiceCollection services) => services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        public void ConfigureServices(IServiceCollection services) => services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
